a7961fa003bd4024a7648703b086b18a0fa0c5f8
Gui2800 <44672949+Gui2800@users.noreply.github.com>
2018-12-19 19:12:39 -0300

    Update Servidor.js

--- Servidor.js
+++ Servidor.js
@@ -20,3 +20,62 @@ app.get('/', function(req, res){
 app.listen(3000, () => {
     console.log('Escutando em localhost:3000');
   })
+
+-------------------------------
+<!DOCTYPE html>
+html(lang="en")
+    head
+        meta(charset="UTF-8")
+        meta(name="viewport", content="width=device-width, initial-scale=1.0")
+        meta(http-equiv="X-UA-Compatible", content="ie=edge")
+        title Genius
+        style      
+            include server.css
+    body
+        .main
+            a(class = 'buuton' href="/random?bt=1")
+                .titlecontainer.vermelho#btn1
+                    .inner 
+            a(class = 'buuton' href="/random?bt=2")
+                .titlecontainer.amarelo#btn2 
+                    .inner
+            a(class = 'buuton' href="/random?bt=3")
+                .titlecontainer.verde#btn3
+                    .inner  
+            a(class = 'buuton' href="/random?bt=4")
+                .titlecontainer.azul#btn4
+                    .inner 
+        script.
+            var $ = function(el) { return document.getElementById(el) };
+            var ordem = [!{ordem}]; // aqui vem um valor do node
+            // se o valor tiver preenchido, entao vai fazer a animacao
+            function acende(el) {
+                // faz o elemento "el" acender
+                document.getElementById(el.toString()).className = ('titlecontainer black')
+                console.log("acende " + el);
+                setTimeout(function() {
+                    // "apaga" o elemento "el"
+                    console.log('xxxx = ', el, el == 'btn1');
+                    if(el == 'btn1') {
+                        document.getElementById(el.toString()).className = ('titlecontainer vermelho')
+                    } else if (el == 'btn2') {
+                        document.getElementById(el.toString()).className = ('titlecontainer amarelo')
+                    } else if (el == 'btn3') {
+                        document.getElementById(el.toString()).className = ('titlecontainer verde')
+                    } else if (el == 'btn4') {
+                        document.getElementById(el.toString()).className = ('titlecontainer azul')
+                    } else {
+                        console.log('nenhum');
+                    }
+                    console.log("apaga " + el);
+                }, 800);
+            }
+            
+            var i = 0;
+            console.log(ordem);
+            setInterval(function() {
+                if (i < ordem.length) {
+                    acende(`btn${ordem[i]}`);
+                    i++;
+                }
+            }, 1000);
