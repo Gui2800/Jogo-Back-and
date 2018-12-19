91a7a7d69e1c77f86e227e721e980d6a1380e9c1
Gui2800 <44672949+Gui2800@users.noreply.github.com>
2018-12-19 19:14:14 -0300

    Update Servidor.js

--- Servidor.js
+++ Servidor.js
@@ -22,6 +22,10 @@ app.listen(3000, () => {
   })
 
 -------------------------------
+
+    
+PUG    
+    
 <!DOCTYPE html>
 html(lang="en")
     head
@@ -79,3 +83,169 @@ html(lang="en")
                     i++;
                 }
             }, 1000);
+-------------------------
+    
+SERVIDOR
+
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const url = require('url')
+
+const app = express();
+
+app.use(cookieParser());
+app.use(session({secret: 'H A H A'}));
+
+const pug = require('pug');
+const compiledFunction = pug.compileFile('server.pug');
+var number = Math.floor(Math.random() * 4);
+
+app.get('/teste', (req, res) => {
+    
+})
+
+app.get('/random', function(req, res){
+    var parts = url.parse(req.url, true);
+    var query = parts.query;
+    var bt = query.bt
+    console.log(bt);
+    
+    console.log("sess:");
+    for (k in req.session) {
+        console.log(`sess[${k}] = ${req.session[k]}`);
+    }
+    console.log("--");
+    
+    var click = true;
+    //var clique = req.session.click;
+    if (!req.session.click) {
+        req.session.click = [];
+    }
+    console.log(req.session.click);
+    //var valorInput = letras.vi;
+    if (!req.session.colors) {
+        req.session.colors = [];
+    }
+    // Verificar a quantidade de clicks
+    if(!req.session.num_click) {
+        req.session.num_click = 0;
+    }
+    
+    if (bt) {
+        req.session.num_click++;
+        req.session.click.push(bt);
+    }
+    
+    //Pra quebra a url para fazer o segundo array
+    var sessao = req.session.colors;
+    var teste = true;   
+    console.log(req.session.num_click);
+    console.log(req.session.click);
+    console.log(req.session.colors);
+    if(req.session.num_click == req.session.colors.length) {
+        for (let i = 0; i < sessao.length; i++) {
+            console.log(sessao);
+            console.log(i);
+            if (req.session.colors[i] == req.session.click[i] && teste){
+                console.log('acertou');
+            }else {
+                console.log('errou');
+                //Se errar encerra a sessão 
+                //não Destruir sessão
+                //req.session.destroy(compiledFunction, () => {
+                //    // teste = false;
+                //});
+                teste = false;
+            }
+        }
+        var addColor = teste;
+        if (addColor) {
+            var n = 1 + (Math.floor(Math.random() * 4)% 4);
+            req.session.colors.push(n);
+                console.log(req.session.colors);
+                req.session.num_click = 0;
+                req.session.click = [];
+        }  else {
+            teste = false;
+            req.session.colors = [];
+            req.session.num_click = 0;
+            req.session.click = [];
+            res.send("Errou <a href=\"/random\">voltar</a>");
+            return;
+        }
+    }
+    console.log("sess:");
+    for (k in req.session) {
+        console.log(`sess[${k}] = ${req.session[k]}`);
+    }
+    console.log("--");
+    console.log("--");
+    res.send(compiledFunction({
+        ordem: (req.session.num_click == 0 ? req.session.colors.join(",") : "")
+    }));
+});
+
+app.listen(3000, () => {
+    console.log('Escutando em localhost:3000');
+})
+
+-----------------------
+    
+CSS
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+a {
+    display: inline-block;
+    margin-top: -4px;
+}
+.main {
+    padding-top: 4px;
+    width: 200px;
+    height: 200px;
+}
+.titlecontainer {
+    border: none;
+    width: 100px;
+    height: 100px;
+    display: inline-block;
+    text-align: center;
+}
+.vermelho {
+    background-color: red;
+    border-top-left-radius: 100%;
+}
+/*.vermelho .inner {
+    border-top-left-radius: 100%;
+    background: white;
+    width: 70%;
+    height: 70%;
+    position: relative;
+    top: 30%;
+    left: 30%;
+}*/
+.amarelo {
+    background-color: yellow;
+    border-top-right-radius: 100%;
+}
+
+.verde {
+    background-color: green;
+    border-bottom-left-radius: 100%;
+}
+.azul {
+    background-color: blue;
+    border-bottom-right-radius: 100%;
+}
+
+/*-------------*/
+
+.black1{
+    background-color: white;
+    border-radius: 100%;
+}
